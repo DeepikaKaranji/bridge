7ca36d86f883c314ecb2604ceb56cc935b915111
code-brewer <broozewang@yahoo.com>
2025-11-24 16:31:15 +0800

    Remove personal-dev folder as it's messy and developer-specific
    This folder was just for developer's own testing and notes, but it got too messy and isn't needed in the repo anymore. Removing it keeps things tidy.
    
    Signed-off-by: code-brewer <broozewang@yahoo.com>

--- contracts/contracts_ethereum/ope_dev/0_address.js
+++
@@ -1,7 +0,0 @@
-const { getWalletfromWords} = require('./common');
-
-const main = async () => {
-    let words = "deer funny trip dress siren crane build stomach thunder setup yellow audit";
-    getWalletfromWords(words)
-}
-main()
